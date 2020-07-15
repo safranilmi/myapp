a4d26e14c8c414dd8ee5436021d1c4990bb11d6a
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-07-15 13:53:24 +0700

    Update Blog.php

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -7,9 +7,10 @@ class Blog extends CI_Controller
 {
 	public function index($nama, $goldarah, $alamat)
 	{
-		echo "Selamat Datang $nama <br>";
-		echo "Golongan Darah Anda $goldarah <br>";
-		echo "Alamat Anda di $alamat <br>";
+		$data['nama'] = $nama;
+		$data['goldarah'] = $goldarah;
+		$data['alamat'] = $alamat;
+		$this->load->view('blog', $data);
 	}
 	
 }
